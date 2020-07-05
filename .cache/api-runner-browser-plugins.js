49f50b39d8e056855c94f5eca5c68decb1b62e65
Min Ji Lee <minjilee1707@gmail.com>
2020-07-05 17:31:36 +0100

    add main image and name

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,4 @@
+module.exports = [{
+      plugin: require('/home/minjilee1707/repos/minjilee1707.github.io/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ .cache/async-requires.js
@@ -0,0 +1,12 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("/home/minjilee1707/repos/minjilee1707.github.io/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("/home/minjilee1707/repos/minjilee1707.github.io/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-about-me-js": () => import("/home/minjilee1707/repos/minjilee1707.github.io/src/pages/about-me.js" /* webpackChunkName: "component---src-pages-about-me-js" */),
+  "component---src-pages-index-js": () => import("/home/minjilee1707/repos/minjilee1707.github.io/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
+}
+
+exports.data = () => import("/home/minjilee1707/repos/minjilee1707.github.io/.cache/data.json")
+
